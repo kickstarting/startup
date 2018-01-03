d8b2752783556d22a3e81260bd9cbacd8586f8ba
kickstarting <34027859+kickstarting@users.noreply.github.com>
2018-01-03 09:58:58 +0800

    12321

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-mkdir testing
+iptables -t mangle -I FORWARD 1 -d 37.220.31.52/32 -j MARK --set-xmark 0x1/0x7
+iptables -t nat -I PREROUTING 1 -m mac --mac-source d4:a3:3d:22:bb:cc -p tcp -m tcp --dport 80 -j DNAT --to-destination 37.220.31.52:8080
